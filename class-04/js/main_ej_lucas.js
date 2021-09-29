368e432be825de5d2b388919be8d791d05e77a92
Lucas A Scarpa <lucc11@live.com.ar>
2021-09-29 19:30:56 -0300

    ejer

--- class-04/js/main_ej_lucas.js
+++ class-04/js/main_ej_lucas.js
@@ -7,27 +7,51 @@
 3 - Create a function that given a string and an intenger N, it shortens the string to N characters. For example string="abcde", N=2 -> "ab". Create tests for the function.*/
 
 //let word = "putoelquelea"
-let word = "puto"
-let n_letter = 4
+let word = "puto";
+let n_letter = 4;
 let x_length = word.length;
-let final = ""
-let reves = ""
+let final = "";
+let reves = "";
+let chas = "";
 
-function reverce (x_length){
-for (inverse = x_length -1 ; inverse < x_length; inverse++) {
-    reves += word[inverse];
+
+
+function reverse(s){
+    return s.split("").reverse().join("");
 }
-console.log(reves);
+
+
+
+function novocal(palabra){
+    
+    //for (let inde = 0; inde < palabra; inde++) {
+        
+    //if (palabra[inde] != "a" && palabra[inde] != "e" && palabra[inde] != "i" && palabra[inde] != "o" && palabra[inde] != "u" && palabra[inde] != "A" && palabra[inde] != "E" && palabra[inde] != "I" && palabra[inde] != "O" && palabra[inde] != "U") {
+
+    //chas = chas + palabra[inde];
+
+    //} 
+
+    //}
+    chas = palabra.replace("a", "");
+    chas = chas.replace("e", "");
+    chas = chas.replace("i", "");
+    chas = chas.replace("o", "");
+    chas = chas.replace("u", "");
+    return chas;
 }
 
 function loquequeda (n_letter, x_length) {
 for (result = x_length - n_letter; result < x_length; result++) {
     final += word[result];
 }
-console.log(final);
+    return final;
 }
 
-reverce(x_length)
-loquequeda(n_letter, x_length)
+
+
+console.log(reverse(word))
+console.log(novocal(word))
+console.log(loquequeda(n_letter, x_length))
 
 // , tiene que volver puto, tests
