c6dceeae4f1374539c3304fcba44e3f07bc9889f
lscarpa <lucc11@live.com.ar>
2022-10-27 12:29:44 -0300

    exercise 04-02 complete

---
+++ lucas/src/exercise-04/exercise-04-02.ts
@@ -0,0 +1,22 @@
+import { assertEqualArrays } from "../utils_for_tests";
+
+function multipleByTwo(num: number): number {
+    return num * 2;
+}
+
+function map<Type>(elements: Array<number>, _function_multiplication: (num: Type) => number): Array<number>{ //_ me lo recomienda el quickfix
+    let output = [];
+    for (const numero of elements){
+        output.push(multipleByTwo(numero));
+    }
+    return output;
+}
+
+function testMap(): void{
+    assertEqualArrays(map([10, 500, 2], multipleByTwo), [20, 1000, 4]);
+    assertEqualArrays(map([-100, -1, 0], multipleByTwo), [-200, -2, 0]);
+    assertEqualArrays(map([0.4, 0.5], multipleByTwo), [0.8, 1]);
+    //assertEqualArrays(map(["keso", "1", "ana", "jamon"], multipleByTwo), ["keso", "jamon"]); que pasaria ?
+  
+    console.log("===All test passed===");
+  }
