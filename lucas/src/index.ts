3d4fa18e8039c5a1a032707126daa87c1f6c4bd8
lscarpa <lucc11@live.com.ar>
2022-11-06 17:08:55 -0300

    exercise-5-1 complete not beauty

--- lucas/src/index.ts
+++ lucas/src/index.ts
@@ -1,9 +1,9 @@
 import { testFilter } from "./exercise-04/exercise-04-01";
 import { testMap } from "./exercise-04/exercise-04-02";
 import { testNormalize } from "./exercise-04/exercise-04-03";
-import { isValidSymbolCombination } from "./exercise-05/exercise-05-01";
+import { testExer5 } from "./exercise-05/exercise-05-01";
 
 testFilter();
 testMap();
 testNormalize();
-isValidSymbolCombination("{[()]}");
+testExer5();
